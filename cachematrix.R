c4fa38cb57e8cecf19dc8f5a8f4be4101703189a
Glynn Durham <glynn@cloudera.com>
2014-04-17 20:16:01 -0500

    Implemented makeCacheMatrix() and cacheSolve() functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Create a structure from a matrix that carries its own inverse, if set with setinverse().
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverseX <- NULL
+        set <- function(y) {
+                x <<- y
+                inverseX <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(theInverse) inverseX <<- theInverse
+        getinverse <- function() inverseX
+        list(set = set, 
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Obtain inverse matrix of a CacheMatrix structure, or, if does not exist, 
+##      then compute the inverse and store in the CacheMatrix structure.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inverseX <- x$getinverse()
+        if(!is.null(inverseX)) {
+                return(inverseX)
+        }
+        data <- x$get()
+        theInverse <- solve(data, ...)
+        x$setinverse(theInverse)
+        theInverse
 }
